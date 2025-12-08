29d006665bc20c5cc6a50b33bdf8b94f324e1595
Jeff <140712389+manemajef@users.noreply.github.com>
2025-12-09 01:34:34 +0200

    init

---
+++ app/v2/page.tsx
@@ -0,0 +1,33 @@
+import Hero from "@/components/sections/hero";
+import Models from "@/components/sections/models";
+import Intro from "@/components/sections/intro";
+// import Features from "@/components/sections/features";
+import FAQ from "@/components/sections/faq";
+import Pricing from "@/components/sections/pricing";
+import Footer from "@/components/sections/footer";
+import Features from "@/components/sections/features2";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <div className="max-w-6xl mx-auto">
+        <Intro />
+
+        <div id="guide">
+          <Models />
+        </div>
+        <Features />
+        <div id="pricing">
+          <Pricing />
+        </div>
+        <div id="faq">
+          <FAQ />
+        </div>
+        <div id="footer">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
